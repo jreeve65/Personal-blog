b3efadab861ea1e4b1557064d17d526e40b2c321
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-02 14:31:17 -0500

    added form to page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,6 @@
-console.log('this is my form js');
+const form = document.querySelector("form");
+const handleFormSubmit = function(event){
+    event.preventDefault();
+    console.log("form submitted");
+}
+form.addEventListener("submit", handleFormSubmit)
