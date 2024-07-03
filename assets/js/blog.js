47e7053bd2f007c89b36afbb68de889112526450
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-03 12:04:00 -0500

    created elements for use in  function displayData in blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,10 +2,16 @@ const body = document.body;
 //creates a cost varible to read data in local storage
 const blogData = getLocalStorage();
 function displayData() {
-    //create element to contain the post : what to call???
-    //create child header element with the text content of the blog title
+    //create element to contain the post we will appened everything later in function to this parent
+    const postContainer = document.createElement('section');
+    //create child header element with the text content of the blog title in an h1 element
+    const postHeader = document.createElement('header');
+    const h1El = document.createElement('h1');
+    
     // create a child element that contains the text of the content
+    const postContent = document.createElement('p');
     // create child element that stores text posted by and then the posters user name
+    const postUsername = document.createElement('p');
 }
 
 // for (let i = 0; i < blogData.length; i++) {
