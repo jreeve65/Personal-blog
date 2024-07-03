19b547fbe735a64890e4cc53f0393b7a08e62bf9
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-03 14:22:22 -0500

    updated comments to expain logic

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,19 +2,21 @@ const body = document.body;
 //creates a cost varible to read data in local storage
 const blogData = getLocalStorage();
 function displayData(index) {
-    //create element to contain the post we will appened everything later in function to this parent
+    //creates element to contain the post we will appened everything later in function to this parent
     const postContainer = document.createElement('section');
-    //create child header element with the text content of the blog title in an h1 element
+    //creates child header element with the text content of the blog title in an h1 element
     const postHeader = document.createElement('header');
     const h1El = document.createElement('h1');
     h1El.textContent = blogData[index].blogTitle;
-    // create a child element that contains the text of the content
+    // creates a child element that contains the text of the content
     const postContent = document.createElement('p');
     postContent.textContent = blogData[index].content;
-    // create child element that stores text posted by and then the posters user name
+    // creates child element that stores text posted by and then the posters user name
     const postUsername = document.createElement('p');
-    postUsername.textContent = `posted by ${blogData[index].userName}`;
+    postUsername.textContent = `Posted by: ${blogData[index].userName}`;
+    //appends text of newly created elements to post container
     postContainer.append(postHeader,h1El,postContent,postUsername);
+    //appends postcontainer to the page.
     body.append(postContainer);
 }
 
