2516ffb49a0bf9b4b0943719be9111d39e5be629
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-02 20:32:17 -0500

    adjusted code so that it pushes objects to a global array

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,26 +2,26 @@ const form = document.querySelector("form");
 const userNameInput = document.querySelector("#username"); // takes submitted form username
 const blogTitleInput = document.querySelector("#blogtitle");//takes the title of the form
 const contentInput = document.querySelector("#blogcontent");// takes the blog content of the form
+const posts = [];
 const handleFormSubmit = function (event) { //takes the form submission, stores it in an object and then takes the object to local storage.
-    const posts = [];
+
     event.preventDefault();
     if (userNameInput.value == "" || blogTitleInput == "" || contentInput == "") {
         console.log("must have all fields filled out");
         return;
     }
+    //object created to store all values in the array.
     const newPost = {
         userName: userNameInput.value.trim(),
         blogTitle: blogTitleInput.value.trim(),
         content: contentInput.value.trim(),
     }
-    console.log(newPost.userName);
-    console.log(newPost.blogTitle);
-    console.log(newPost.content);
+    // console.log(newPost.userName);
+    // console.log(newPost.blogTitle);
+    // console.log(newPost.content);
     posts.push(newPost);
-    console.log("these are retrieved from the posts array----------------------------------");
-    console.log(posts[0].userName);
-    console.log(posts[0].blogTitle);
-    console.log(posts[0].content);
+    
 }
-
-form.addEventListener("submit", handleFormSubmit)
+form.addEventListener("submit", handleFormSubmit);
+// console.log(posts[0].blogTitle);
+// console.log(posts[0].content);
