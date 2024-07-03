8e8bef324cf7a2aa3f3e2ab142806150b73f3620
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-02 19:35:54 -0500

    .trim property to objec

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,17 @@
 const form = document.querySelector("form");
-const userNameInput = document.querySelector("#username");
-const blogTitleInput = document.querySelector("#blogtitle");
-const contentInput = document.querySelector("#blogcontent");
-const handleFormSubmit = function(event){
+const userNameInput = document.querySelector("#username"); // takes submitted form username
+const blogTitleInput = document.querySelector("#blogtitle");//takes the title of the form
+const contentInput = document.querySelector("#blogcontent");// takes the blog content of the form
+const handleFormSubmit = function(event){ //takes the form submission, stores it in an object and then takes the object to local storage.
     event.preventDefault();
     if(userNameInput.value ==""||blogTitleInput ==""||contentInput==""){
         console.log("must have all fields filled out");
         return;
     }
     const newPost = {
-        userName: userNameInput.value,
-        blogTitle: blogTitleInput.value,
-        content:contentInput.value,
+        userName: userNameInput.value.trim(),
+        blogTitle: blogTitleInput.value.trim(),
+        content:contentInput.value.trim(),
     }
     console.log(newPost.userName);
     console.log(newPost.blogTitle);
