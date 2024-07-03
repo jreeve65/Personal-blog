6f9d1221ba9a53f6c27f498b5f75623cbeb96fe5
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-03 11:43:35 -0500

    added pseudo code to blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,14 @@
+const body = document.body;
+//creates a cost varible to read data in local storage
 const blogData = getLocalStorage();
-
-for (let i = 0; i < blogData.length; i++) {
-    console.log(blogData[i]);
+function displayData() {
+    //create element to contain the post : what to call???
+    //create child header element with the text content of the blog title
+    // create a child element that contains the text of the content
+    // create child element that stores text posted by and then the posters user name
 }
+
+// for (let i = 0; i < blogData.length; i++) {
+//     console.log(blogData[i]);
+// }
 
