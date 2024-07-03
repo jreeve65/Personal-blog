dc0930f02ce99094dc56345108d662edbdd69876
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-02 19:17:22 -0500

    added logic to form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,18 @@
 const form = document.querySelector("form");
+const userNameInput = document.querySelector("#username");
+const blogTitleInput = document.querySelector("#blogtitle");
+const contentInput = document.querySelector("#blogcontent");
 const handleFormSubmit = function(event){
     event.preventDefault();
-    console.log("form submitted");
+    const newPost = {
+        userName: userNameInput.value,
+        blogTitle: blogTitleInput.value,
+        content:contentInput.value,
+    }
+    console.log(newPost.userName);
+    console.log(newPost.blogTitle);
+    console.log(newPost.content);
+    return;
+   
 }
 form.addEventListener("submit", handleFormSubmit)
