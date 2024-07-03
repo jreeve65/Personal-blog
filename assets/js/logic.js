bf0dfeed105d440163d130d37d1e348c18606dd4
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-03 18:53:33 -0500

    added note comment for future reference

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,5 @@ const getLocalStorage = function () {
     const dataToStore = JSON.parse(localStorage.getItem("posts"));//read from the data base
     return dataToStore || [];    // returns all the data in database or an empty array.
 }
+
+//Use api ins number 11 Event-listener as a reference for theme switcher logic.
