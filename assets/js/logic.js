bc74a1e2d8fedcd992b2a5c772f57bed298b7c5c
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-03 19:15:20 -0500

    added some sample code to reference from inclass activity

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,3 +4,23 @@ const getLocalStorage = function () {
 }
 
 //Use api ins number 11 Event-listener as a reference for theme switcher logic.
+
+// const themeSwitcher = document.querySelector('#theme-switcher');
+// const container = document.querySelector('.container');
+
+// // Set default mode to dark
+// let mode = 'dark';
+
+// // Listen for a click event on toggle switch
+// themeSwitcher.addEventListener('click', function () {
+//   // If mode is dark, apply light background
+//   if (mode === 'dark') {
+//     mode = 'light';
+//     container.setAttribute('class', 'light');
+//   }
+//   // If mode is light, apply dark background
+//   else {
+//     mode = 'dark';
+//     container.setAttribute('class', 'dark');
+//   }
+// });
