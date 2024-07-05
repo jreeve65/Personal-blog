fc0dc332c4eadcc3f3ea63ad10795157207fe9b0
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-04 23:49:41 -0500

    added a theme switcher debugging needed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,68 @@
+
+//read function
+
+
 const getLocalStorage = function () {
     const dataToStore = JSON.parse(localStorage.getItem("posts"));//read from the data base
     return dataToStore || [];    // returns all the data in database or an empty array.
 }
-
-//Use api ins number 11 Event-listener as a reference for theme switcher logic.
-
-// const themeSwitcher = document.querySelector('#theme-switcher');
-// const container = document.querySelector('.container');
-
-// // Set default mode to dark
+//sets up for theme switch
 // let mode = 'dark';
+// let body = document.querySelector('.dark_body');
+// let elements = document.querySelector('.dark');
+// let sun = document.querySelector('#sun_dark');
+// let container = document.querySelector('.container');
+// let card = document.querySelector('.card');
+// let pageHeader = document.querySelector('#dark_pageHeader');
+// let themeSwitcher = document.querySelector('#theme-switcher');
 
-// // Listen for a click event on toggle switch
-// themeSwitcher.addEventListener('click', function () {
-//   // If mode is dark, apply light background
-//   if (mode === 'dark') {
-//     mode = 'light';
-//     container.setAttribute('class', 'light');
-//   }
-//   // If mode is light, apply dark background
-//   else {
-//     mode = 'dark';
-//     container.setAttribute('class', 'dark');
-//   }
-// });
+
+//theme switch logic
+
+// handleThemeSwitch = function (event) {
+//     // event.preventDefault();
+//     if (mode === 'dark') {
+//         // for reverting assignments back if light mode has already been selected before.
+//         mode = 'light';
+
+//         body = document.querySelector('.dark_body');
+//         elements = document.querySelector('.dark');
+//         sun = document.querySelector('#sun_dark');
+//         container = document.querySelector('.container');
+//         card = document.querySelector('.card');
+//         pageHeader = document.querySelector('#dark_pageHeader');
+        
+//         //for setting new classes after removing
+//         body.setAttribute('class', 'light_body');
+//         elements.setAttribute('class', 'light');
+//         sun.setAttribute('id', 'sun_light');
+//         card.setAttribute('class', 'card light');
+//         pageHeader.setAttribute('id', 'light_pageHeader');
+//         container.setAttribute('class', 'container light');
+//         // form.setAttribute('class','light');
+
+//     }
+//     else {
+//         // for setting queries to select the right elements after they have been switched.
+//         mode = 'dark';
+//         body = document.querySelector('.light_body');
+//         elements = document.querySelector('.light');
+//         sun = document.querySelector('#sun_light');
+//         container = document.querySelector('.container');
+//         card = document.querySelector('.card');
+//         pageHeader = document.querySelector('#light_pageHeader');
+
+//         body.setAttribute('class', 'dark_body');
+//         elements.setAttribute('class', 'dark');
+//         sun.setAttribute('id', 'sun_dark');
+//         card.setAttribute('class', 'card dark');
+//         pageHeader.setAttribute('id', 'dark_pageHeader');
+//         container.setAttribute('class', 'container dark');
+//         // form.setAttribute('class','dark');
+        
+//     }
+
+
+// }
+// themeSwitcher.addEventListener('click',handleThemeSwitch);
+
